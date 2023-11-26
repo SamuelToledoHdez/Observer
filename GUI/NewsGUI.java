394461d10e6c61832edc71586005b9cb241e2f9e
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-11-26 16:52:38 +0000

    Panel Noticias mejorado

--- GUI/NewsGUI.java
+++ GUI/NewsGUI.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 
 public class NewsGUI extends JFrame {
     public NewsGUI(ArrayList<String> noticias, String tituloPagina) {
-        //configurarLookAndFeel();
+        configurarLookAndFeel();
         // Configurar la ventana
         setTitle("Noticias - " + tituloPagina);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,14 +52,14 @@ public class NewsGUI extends JFrame {
         JPanel panelCabecera = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JLabel labelCabecera = new JLabel(tituloPagina);
         labelCabecera.setFont(new Font("Roboto", Font.BOLD, 18));
+        panelCabecera.add(labelCabecera);
 
-        ImageIcon iconoLogo = getLogo(50, 150);
+        ImageIcon iconoLogo = getLogo();
         if (iconoLogo != null) {
             JLabel labelLogo = new JLabel(iconoLogo);
             panelCabecera.add(labelLogo);
         }
 
-        panelCabecera.add(labelCabecera);
         return panelCabecera;
     }
 
@@ -94,14 +94,15 @@ public class NewsGUI extends JFrame {
         labelTituloNoticia.setForeground(Color.BLACK);
         labelTituloNoticia.setCursor(new Cursor(Cursor.HAND_CURSOR));
         labelTituloNoticia.setFont(new Font("Roboto", Font.BOLD, 14));
-        labelTituloNoticia.addMouseListener(new NoticiaMouseListener(noticia));
+        labelTituloNoticia.addMouseListener(new NoticiaMouseListener(noticia, extraerTituloNoticia(noticia)));
     }
 
-    ImageIcon getLogo(int h, int w) {
+    ImageIcon getLogo() {
         URL urlLogo = getClass().getResource("/logo.png");
         try {
+            assert urlLogo != null;
             ImageIcon originalIcon = new ImageIcon(urlLogo);
-            Image resizedImage = originalIcon.getImage().getScaledInstance(w, h, Image.SCALE_SMOOTH);
+            Image resizedImage = originalIcon.getImage().getScaledInstance(130, 35, Image.SCALE_SMOOTH);
             return new ImageIcon(resizedImage);
         } catch (Exception e) {
             e.printStackTrace();
@@ -125,27 +126,58 @@ public class NewsGUI extends JFrame {
         return lineas.length > 0 ? lineas[0].trim() : noticia.trim();
     }
 
-    private class NoticiaMouseListener extends MouseAdapter {
-        private String noticia;
+    private static class NoticiaMouseListener extends MouseAdapter {
+    final private String noticia;
+    final private String tituloNoticia;
 
-        public NoticiaMouseListener(String noticia) {
+        public NoticiaMouseListener(String noticia, String tituloNoticia) {
             this.noticia = noticia;
+            this.tituloNoticia = tituloNoticia;
         }
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            // Abrir una nueva ventana para mostrar la noticia completa
-            JFrame ventanaNoticia = new JFrame("Noticia Completa");
-            ventanaNoticia.setSize(400, 300);
+            // Crear una nueva ventana para mostrar la noticia completa
+            JFrame ventanaNoticia = new JFrame(tituloNoticia);
+            ventanaNoticia.setSize(600, 400);
+            ventanaNoticia.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cerrar solo la ventana actual al cerrar
+
+            // Crear un panel principal con BorderLayout
+            JPanel panelPrincipal = new JPanel(new BorderLayout());
+
+            // Crear un área de texto estilizada para la noticia
+            JLabel labelTituloNoticia = new JLabel("<html><div style='width: 400px; text-align: center;'>" + tituloNoticia + "</div></html>");
+            labelTituloNoticia.setFont(new Font("Roboto", Font.BOLD, 18)); // Ajustar la fuente y el tamaño
+            labelTituloNoticia.setBackground(new Color(222, 222, 216)); // Color de fondo suave
+            labelTituloNoticia.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Ajustar el margen interno
+            labelTituloNoticia.setOpaque(true); // Hacer visible el fondo
+            labelTituloNoticia.setPreferredSize(new Dimension(400, labelTituloNoticia.getPreferredSize().height)); // Limitar el ancho
+            panelPrincipal.add(labelTituloNoticia, BorderLayout.NORTH);
 
             JTextArea textoNoticia = new JTextArea(noticia);
             textoNoticia.setLineWrap(true);
             textoNoticia.setWrapStyleWord(true);
             textoNoticia.setEditable(false);
+            textoNoticia.setBackground(new Color(220, 220, 216));
+            textoNoticia.setFont(new Font("Roboto", Font.PLAIN, 16)); // Ajustar la fuente y el tamaño
 
-            ventanaNoticia.add(new JScrollPane(textoNoticia));
+            // Agregar el área de texto a un JScrollPane para permitir el desplazamiento
+            JScrollPane scrollPane = new JScrollPane(textoNoticia);
+            scrollPane.setBorder(BorderFactory.createEmptyBorder()); // Eliminar el borde del JScrollPane
+            panelPrincipal.add(scrollPane, BorderLayout.CENTER);
+
+            // Configurar el fondo y el título resaltado
+            panelPrincipal.setBackground(new Color(222, 222, 216)); // Color de fondo suave
+            textoNoticia.setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(new Color(149, 148, 138), 2), // Borde resaltado
+                    BorderFactory.createEmptyBorder(10, 10, 10, 10) // Margen interno
+            ));
+
+            // Agregar el panel principal a la ventana de la noticia
+            ventanaNoticia.add(panelPrincipal);
             ventanaNoticia.setVisible(true);
         }
+
     }
 
 }
